ca6714ba8ccd793497ffa5378c8235436eb0f4eb
Chris Roberts <croberts@luc.id>
2017-04-24 13:47:28 -0500

    added lib fxn

--- mylib.py
+++ mylib.py
@@ -1,7 +1,18 @@
-
+import math
 
 def factorial(num):
     for index in range(num, 0, -1):
         num = num * index
     return num
+
+def is_prime(num):
+    if n == 0 or n == 1:
+        return False
+    if n % 2 == 0 and n > 2:
+        return False
+    for i in range(3, math.ceil(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 
